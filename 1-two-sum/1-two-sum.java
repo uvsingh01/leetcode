23d55f1b62825335d826c23df09e8a986ca7850a
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-05-05 19:08:14 +0530

    Time: 8 ms (48.42%), Space: 47.3 MB (5.72%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,16 +1,34 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] array=new int[2];
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=1; j<nums.length;  j++ ){
-                int x= nums[i]+nums[j]; //nums = [2,7,11,15], target = 9
-                                                //0 1  2  3
-                if(x==target && i!=j){
-                    array[0]=i;
-                    array[1]=j;
-                }
+        // int arr[]=new int[2];
+        // int start =0;
+        // int end = nums.length -1;
+        // int temp=0;
+        // Arrays.sort(nums);
+        // while(target!=temp){
+        //     temp=0;
+        //     temp=nums[start]+nums[end];
+        //     if(temp>target){
+        //         end--;
+        //     }
+        //     else{
+        //         start++;
+        //     }
+        // }
+        // arr[0]=start;
+        // arr[1]=end;
+        // return arr;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int [] arr = new int[2];
+        for(int i=0; i<nums.length; i++){
+            if(hm.containsKey(target-nums[i])){
+                arr[0]=hm.get(target-nums[i]);
+                arr[1]=i;
+            }
+            else{
+                hm.put(nums[i],i);
             }
         }
-        return array;
+        return arr;
     }
 }
