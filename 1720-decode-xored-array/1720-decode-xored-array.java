764c346766f9bf053476011786f918b7725a0d01
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-05-06 17:24:30 +0530

    Time: 2 ms (67.61%), Space: 56.3 MB (39.05%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] decode(int[] a, int b) {
+        int []arr = new int[a.length+1];
+        int res =0;
+        int sum = b;
+        arr[0]=sum;
+        for(int i=0; i<a.length; i++){
+            sum= sum^a[i];
+            arr[i+1]=sum;
+        }
+        return arr;
+    }
+}
