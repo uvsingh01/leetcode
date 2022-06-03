4f5ccabb412dc808bfd6c453f26da4f42365249b
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-06-03 17:34:46 +0530

    Time: 2 ms (65.09%), Space: 41.1 MB (60.57%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,15 +1,15 @@
 class Solution {
     public int reverse(int x) {
         int sum=0;
-        int temp=x;
+        // int temp=x;
         
-        while(temp!=0){
-            int rem= temp%10;
+        while(x!=0){
+            int rem= x%10;
             sum=(10*sum)+rem;
             if(rem!=(sum%10)){
                 return 0;
             }
-            temp=temp/10;
+            x=x/10;
             
         }
         return sum;
