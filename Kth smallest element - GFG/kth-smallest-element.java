3803abcabfcb70c73b3279c6e003c98e664af32a
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-06-08 14:42:09 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.java
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -35,8 +35,8 @@ class GFG {
 class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) 
     { 
-        // qs(arr,0,arr.length-1);
-        Arrays.sort(arr);
+        mergesort(arr,0,arr.length-1);
+        // Arrays.sort(arr);
         return arr[k-1];
     }
     public static void qs(int []arr, int start, int end){
@@ -65,5 +65,42 @@ class Solution{
         int temp= arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+    static void mergesort(int[]arr, int start, int end){
+        if(start<end){
+            int mid=start+(end-start)/2;
+            mergesort(arr,start,mid);
+            mergesort(arr,mid+1,end);
+            merger(arr,start,mid,end);
+        }
+    }
+    static void merger(int[]arr, int start, int mid ,int end){
+        int m = mid-start+1;
+        int n = end-mid;
+        int a[]=new int[m];
+        int b[]=new int[n];
+        int k=0;
+        for (int i = start; i <=mid ; i++) {
+            a[k++]=arr[i];
+        }
+        k=0;
+        for (int i = mid+1; i <=end ; i++) {
+            b[k++]=arr[i];
+        }
+        int i=0; int j=0; k=start;
+        while(i<m && j<n){
+            if(a[i]<b[j]){
+                arr[k++]=a[i++];
+            }
+            else{
+                arr[k++]=b[j++];
+            }
+        }
+        while(i<m){
+            arr[k++]=a[i++];
+        }
+        while(j<n){
+            arr[k++]=b[j++];
+        }
     }
 }
